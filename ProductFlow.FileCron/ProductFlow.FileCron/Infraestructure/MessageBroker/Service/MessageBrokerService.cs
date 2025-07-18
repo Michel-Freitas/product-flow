b710a2b65c420bb0acfbb121dec7f8713c3c8334
Michel Freitas <michelfreitas85@outlook.com>
2025-07-18 14:57:47 -0300

    Publishing bath of lines to the queue

--- ProductFlow.FileCron/ProductFlow.FileCron/Infraestructure/MessageBroker/Service/MessageBrokerService.cs
+++ ProductFlow.FileCron/ProductFlow.FileCron/Infraestructure/MessageBroker/Service/MessageBrokerService.cs
@@ -1,26 +1,38 @@
 ﻿using Confluent.Kafka;
 using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
+using ProductFlow.FileCron.Infraestructure.MessageBroker.Dto;
 using ProductFlow.FileCron.Infraestructure.MessageBroker.Interface;
 using ProductFlow.FileCron.Infraestructure.MessageBroker.Settings;
 
 namespace ProductFlow.FileCron.Infraestructure.MessageBroker.Service
 {
-    public class MessageBrokerService : IMessageBrokerService, IDisposable
+    public class MessageBrokerService(IOptions<MessageBrokerSettings> options) : IMessageBrokerService, IDisposable
     {
-        private readonly IConsumer<Ignore, string> _consumer;
+        private readonly IConsumer<Ignore, string> _consumer = CreateConsumer(options);
+        private readonly IProducer<Null, string> _producer = CreateProducer(options);
 
-        public MessageBrokerService(IOptions<MessageBrokerSettings> options)
+        private static IProducer<Null, string> CreateProducer(IOptions<MessageBrokerSettings> options)
+        {
+            var config = new ProducerConfig
+            {
+                BootstrapServers = options.Value.Endpoint
+            };
+
+            return new ProducerBuilder<Null, string>(config).Build();
+        }
+
+        private static IConsumer<Ignore, string> CreateConsumer(IOptions<MessageBrokerSettings> options)
         {
             var config = new ConsumerConfig
             {
                 BootstrapServers = options.Value.Endpoint,
                 GroupId = options.Value.Consumer.GroupId,
                 AutoOffsetReset = AutoOffsetReset.Earliest,
-                // EnableAutoCommit = options.Value.Consumer.EnableAutoCommit
+                EnableAutoCommit = options.Value.Consumer.EnableAutoCommit
             };
 
-            _consumer = new ConsumerBuilder<Ignore, string>(config).Build();
+            return new ConsumerBuilder<Ignore, string>(config).Build();
         }
 
         public T Consume<T>()
@@ -52,6 +64,18 @@ namespace ProductFlow.FileCron.Infraestructure.MessageBroker.Service
         {
             _consumer.Close();
             _consumer.Dispose();
+        }
+
+        public async Task PublishAsync<T>(string topic, T message)
+        {
+            var jsonMessage = JsonConvert.SerializeObject(new EventDto<T>(message));
+
+            var sendMessage = new Message<Null, string>
+            {
+                Value = jsonMessage
+            };
+
+            await _producer.ProduceAsync(topic, sendMessage);
         }
     }
 }
--- ProductFlow.FileCron/ProductFlow.FileCron/UseCase/ProcessFile/Service/ProcessFileService.cs
+++ ProductFlow.FileCron/ProductFlow.FileCron/UseCase/ProcessFile/Service/ProcessFileService.cs
@@ -1,5 +1,7 @@
 ﻿using ProductFlow.FileCron.Domain.Interface.Repository;
 using ProductFlow.FileCron.Infraestructure.MessageBroker.Dto;
+using ProductFlow.FileCron.Infraestructure.MessageBroker.Interface;
+using ProductFlow.FileCron.Infraestructure.MessageBroker.Settings;
 using ProductFlow.FileCron.Infraestructure.Storage.Enums;
 using ProductFlow.FileCron.Infraestructure.Storage.Interface;
 using ProductFlow.FileCron.UseCase.ProcessFile.Dto;
@@ -9,17 +11,18 @@ namespace ProductFlow.FileCron.UseCase.ProcessFile.Service
 {
     public class ProcessFileService(
         IFileRepository fileRepository,
-        IStorageService storageService
+        IStorageService storageService,
+        IMessageBrokerService brokerService
     ) : IProcessFileService
     {
         public async Task ExecuteAsyn(EventDto<FileUploadedEventDto> input)
         {
             var fileEntity = await fileRepository.GetFileById(input.Data.FileId);
-            // TODO: [ ] - Implementar trava para caso a entidade não exista no banco de dados
+            // TODO: Implementar trava para caso a entidade não exista no banco de dados
             var filePath = await storageService.DownloadFile(BucketsEnum.ProductFlow, fileEntity.Path);
-            // TODO: [ ] - Implementar fluxo de arquivo não encontrado
-            // TODO: [ ] - Verificar se o arquivo já teve uma tentativa de processamento, caso sim deve continuar de onde parou
-            // TODO: [ ] - Atualizar o Status do Arquivo para 'WAITING'
+            // TODO: Implementar fluxo de arquivo não encontrado
+            // TODO: Verificar se o arquivo já teve uma tentativa de processamento, caso sim deve continuar de onde parou
+            // TODO: Atualizar o Status do Arquivo para 'WAITING'
 
             using var reader = new StreamReader(filePath);
             var buffer = new List<string>();
@@ -31,22 +34,20 @@ namespace ProductFlow.FileCron.UseCase.ProcessFile.Service
 
                 buffer.Add(line);
 
-                if (buffer.Count == 500)
+                if (buffer.Count == 500) // TODO: - Valor deve ser implementado no Appsettings
                 {
-                    // TODO: [ ] - Salvar no banco os dados do batch enviado para processamento futuro caso trave o processo
-                    // TODO: [ ] - Enviar o batch para o Kafka
+                    // TODO: Salvar no banco os dados do batch enviado para processamento futuro caso trave o processo
+                    await brokerService.PublishAsync<List<string>>(EventsBrokerTopic.FileBatches, buffer);
                     buffer.Clear();
                 }
             }
 
             if (buffer.Count > 0)
             {
-                // TODO: [ ] - Salvar no banco os dados do batch enviado para processamento futuro caso trave o processo
-                // TODO: [ ] - Enviar o batch para o Kafka
+                // TODO: Salvar no banco os dados do batch enviado para processamento futuro caso trave o processo
+                await brokerService.PublishAsync<List<string>>(EventsBrokerTopic.FileBatches, buffer);
                 buffer.Clear();
             }
-
-            // TODO: [ ] - Quando terminar o processamento deve ser avisado o Core API sobre o fim do processamento
         }
     }
 }
